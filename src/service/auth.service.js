95c15bffd8dcc39ae948268fdd2ed5245ded9374
GuuTAR <guitar20122012@gmail.com>
2021-06-07 15:09:52 +0700

    Add auth api

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -20,14 +20,41 @@ export const postRegister = async (data) => {
 
 export const postLogin = async (data) => {
     try {
-        const result =  (await fetch("/login", {
+        const result = await (await fetch("auth/login", {
             method: "POST",
             headers: header,
             body: JSON.stringify(data)
         })).json()
-        sessionStorage.setItem("user_token", result.data.token)
+        sessionStorage.setItem("user_token", result.token)
     }
     catch(error) {
         console.error(error)
+    }
+}
+
+export const postLogout = async () => {
+    try {
+        const result = await fetch("auth/logout", {
+            method: "POST",
+            headers: {
+                ...header,
+                Authorization: `Bearer ${sessionStorage.getItem("user_token")}`
+            },
+        })
+        sessionStorage.removeItem("user_token")
+        return result.json()
+    }
+    catch(error) {
+        console.error(error)
+    }
+}
+
+export const getUserInfo = () => {
+    try {
+        const token = sessionStorage.getItem('user_token')
+        const user_info = jwt_decode(token)
+        return user_info
+    } catch (error) {
+        return error
     }
 }
