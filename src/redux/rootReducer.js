ef30ece17fb47ada35633ca7c02df8d74a88a65c
GuuTAR <guitar20122012@gmail.com>
2021-06-08 19:01:45 +0700

    Add todo reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { counterReducers } from "./test/reducers";
+import { todoReducer } from "./todo/reducers.";
 import { userReducer } from "./user/reducers.";
 
 const rootReducer = combineReducers({
     counter: counterReducers,
     user: userReducer,
+    todo: todoReducer,
 })
 
 export default rootReducer
